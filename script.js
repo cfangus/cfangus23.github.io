75aaef8bd97983c6f83d527f913bfdcf0cc09e55
cfangus <cfangus23@gmail.com>
2020-06-15 23:49:50 +0800

    nav_tag_correct
    nav_tag_correct_first

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ backToTopButton.addEventListener("click",backToTop);
 function backToTop(){
     window.scrollTo(0,0);
 }
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navtag = document.getElementsByClassName('nav-tag')[0]
+
+toggleButton.addEventListener('click', () => {
+    navtag.classList.toggle('active')
+})
